79da5498dc4b0f0582f2456c1226ae6d36b6010e
Shookrun <sukranmamedov999@gmail.com>
2024-05-29 11:25:23 +0400

    link openweatherapp APIKey

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')
+const search = document.querySelector('.search-box button')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetails = document.querySelector('.weather-details')
+
+
+search.addEventListener('click',()=>{
+    const APIKey ='57dc56cd34acd3109836a4701c8166c1'
+     
+})
